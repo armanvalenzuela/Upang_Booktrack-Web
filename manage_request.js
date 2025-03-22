198e53284336331e03dd6238a3197d2b58c22fb5
Justin1208ally <jure.resngit.up@phinmaed.com>
2025-03-22 20:17:01 +0800

    done the request and some other stuff and layout changes

---
+++ manage_request.js
@@ -0,0 +1,100 @@
+// FETCHING COUNT OF BOOKS, UNIF, AND USERS
+fetch('http://localhost/UPBooktrack/admin_get_info.php')
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector('.bookavailability').innerHTML = `
+            <strong> <p id="textbooksavail">Books Available</p> </strong>
+            <img class="bookicon" src="Images/bookicon.png"><br/><br/>
+            <h1>&nbsp; &nbsp;${data.books}</h1>
+        `;
+
+        document.querySelector('.uniformavailability').innerHTML = `
+            <strong> <p>Uniforms Available</p> </strong>
+            <img class="uniformicon" src="Images/uniformlogo.png"><br/><br/>
+            <h1>&nbsp; &nbsp;${data.uniforms}</h1>
+        `;
+
+        document.querySelector('.profile-user').innerHTML = `
+            <strong> <p>Total Users</p> </strong>
+            <img class="usericon" src="Images/profile-user.png"><br/><br/>
+            <h1>&nbsp; &nbsp;${data.users}</h1>
+        `;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+// FOR FETCHING ALL OF THE LOWSTOCK ITEMS (<25)
+fetch('http://localhost/UPBooktrack/admin_get_lowstock.php')
+    .then(response => response.json())
+    .then(data => {
+        const table = document.getElementById('lowstock');
+        
+        // CLEAR ROWS ESCEPT HEADERS
+        table.innerHTML = `
+            <tr>
+                <th colspan="6" bgcolor="white">Low Stock</th>
+            </tr>
+            <tr>
+                <th>Type</th>
+                <th>Department</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Size</th>
+                <th>Stock</th>
+            </tr>
+        `;
+
+        // LOOP THROUGH THE RESPONSE AND ADD THEM AS TABLES CELLS
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${item.type}</td>
+                <td>${item.department}</td>
+                <td>${item.name}</td>
+                <td>${item.gender}</td>
+                <td>${item.size}</td>
+                <td>${item.stock}</td>
+            `;
+            table.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching low stock data:', error));
+
+// FOR FETCHING OUT OF STOCK ITEMS
+
+// FOR FETCHING ALL OF THE OUT OF STOCK ITEMS (STOCK = 0)
+fetch('http://localhost/UPBooktrack/admin_get_outOfstock.php')
+    .then(response => response.json())
+    .then(data => {
+        const table = document.getElementById('outstock');
+
+        // CLEAR ROWS EXCEPT HEADERS
+        table.innerHTML = `
+            <tr>
+                <th colspan="6" bgcolor="white">Out of Stock</th>
+            </tr>
+            <tr>
+                <th>Type</th>
+                <th>Department</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Size</th>
+                <th>Stock</th>
+            </tr>
+        `;
+
+        // LOOP THROUGH THE RESPONSE AND ADD THEM AS TABLE ROWS
+        data.forEach(item => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${item.type}</td>
+                <td>${item.department}</td>
+                <td>${item.name}</td>
+                <td>${item.gender}</td>
+                <td>${item.size}</td>
+                <td>${item.stock}</td>
+            `;
+            table.appendChild(row);
+        });
+    })
+    .catch(error => console.error('Error fetching out-of-stock data:', error));
+
